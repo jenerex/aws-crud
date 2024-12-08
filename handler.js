892e6f143daa5cc12e92799464770c05cf0c3941
jitender <jitenderk908@gmail.com>
2024-12-09 02:09:17 +0530

    chnages

--- handler.js
+++ handler.js
@@ -13,6 +13,14 @@ module.exports.loginAdmin = async (event) => {
     ),
   };
 
+
   // Use this code if you don't use the http event with the LAMBDA-PROXY integration
   // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
 };
+
+exports.register = async (event) => {
+  return {
+    statusCode : 200,
+    body : JSON.stringify({message:"register function has been called"})
+  }
+}
